d0fcc3079dc22dbebe8189ee69815ea697296150
Samuel-Renneke <srenneke@students.unwsp.edu>
2026-03-16 10:05:27 -0500

    Update program_5.py

--- program_5.py
+++ program_5.py
@@ -1,5 +1,16 @@
-# Program #5: Course Info
-# Write a program that has the user input a bunch of course ID and course name pairs.  
-# For example a course ID could be "COS 2005" and the course name could be "Python Programming."   
-# Then ask the user for a subject (like "COS"). 
-# Finally, the program will display the ID and name of all the courses having that subject.
+# Course Info
+# Samuel Renneke, 3/16/2026
+
+classes = {}
+for i in range (1, 11):
+    ID = input("What is the subject of the class you want to add? (ex. COS) ")
+    class_type = input("What is the ID and name of the class you want to add? (ex. 2005 Python Programming) ")
+    classes.setdefault(ID, []).append(class_type)
+
+while True:
+    showing_classes = input("What subject would you like to see classes for? ")
+    print(classes[showing_classes])
+
+    breaking = input("Would you like to see more classes (y/n)? ")
+    if breaking != "y":
+        break
